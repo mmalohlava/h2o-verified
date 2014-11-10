f84e991d5894082e8a69d753be0521823dfde010
mmalohlava <michal.malohlava@gmail.com>
2014-11-09 23:18:02 -0800

    FLatfile mode instead of multicast.

--- bin/runjunit.sh
+++ bin/runjunit.sh
@@ -2,7 +2,9 @@
 
 ROOT_DIR="$( cd ../; pwd )"
 LOCAL_REPO="$ROOT_DIR/.repo"
-
+BASEIP="127.0.0.1"
+BASEPORT="54444"
+TMP="/tmp"
 
 if [ -z $1 ]; then 
 cat <<EOF
@@ -17,8 +19,23 @@ NODES=1
 if [ ! -z $2 ]; then
   NODES=$2
 fi
-JVM="java -Xmx3g -jar target/h2o.jar"
 
+# Generate flatfile
+FLATFILE="$TMP/flatfile_$RANDOM"
+# Record for test
+echo "$BASEIP:$BASEPORT" > $FLATFILE
+# Records for additional nodes
+for N in $(seq 1 $NODES); do
+  echo "$BASEIP:$(($BASEPORT + 2*$N))" >> $FLATFILE
+done
+# Command line
+JAVA="java"
+JVM_OPTS="-Xmx3g"
+H2O_OPTS="-ip $BASEIP -flatfile $FLATFILE -debug"
+H2O_JAR_FILE="target/h2o.jar"
+JVM="$JAVA $JVM_OPTS -jar $H2O_JAR_FILE $H2O_OPTS"
+echo "Command line for JVM: $JVM"
+echo "Flatfile $(cat $FLATFILE)"
 (
   cd "$LOCAL_REPO"
   if [ ! -f "target/h2o.jar" ]; then
@@ -28,16 +45,20 @@ JVM="java -Xmx3g -jar target/h2o.jar"
   echo "Launching $NODES JVMs with H2O"
   PIDS=( )
   for N in $(seq 1 $NODES); do
-    $JVM & PIDS[${#PIDS[*]}]=$!
+    $JVM -port $(($BASEPORT + 2*N))& PIDS[${#PIDS[*]}]=$!
+    sleep 1
   done
   echo "Launched PIDs: ${PIDS[@]}"
   sleep 1
 
-  echo "Launching test..."
-  $JVM -mainClass org.junit.runner.JUnitCore "$TESTCLASS"
+  echo "Launching test... $TESTCLASS"
+  $JAVA $JVM_OPTS -Dh2o.arg.flatfile=$FLATFILE -Dh2o.arg.ip=$BASEIP -Dh2o.arg.port=$BASEPORT\
+    -jar $H2O_JAR_FILE $H2O_OPTS\
+    -mainClass org.junit.runner.JUnitCore "$TESTCLASS"
   
   # Kill JVMs
   for i in "${PIDS[@]}"; do
     kill -9 $i
   done
 )
+
